dc02ee045ae1435c3d35890010349133dd5593fa
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-07-29 18:15:41 +0200

    Created horizontal divider.

--- components/Divider.java
+++ components/Divider.java
@@ -1,18 +1,25 @@
 package net.runelite.client.plugins.tileMapper.components;
 
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
-import lombok.AccessLevel;
+import javax.annotation.Nonnull;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import net.openhft.chronicle.core.annotation.Positive;
 import net.runelite.client.plugins.tileMapper.helpers.ImageLoader;
+import net.runelite.client.plugins.tileMapper.helpers.PiecesTool;
+import net.runelite.client.plugins.tileMapper.helpers.PositionedImage;
+import net.runelite.client.ui.overlay.RenderableEntity;
 
 public interface Divider {
 
-    @Getter
-    @AllArgsConstructor
-    public static class Horizontal {
+    public static class Horizontal implements RenderableEntity {
 
         @AllArgsConstructor
         @Getter
@@ -22,16 +29,37 @@ public interface Divider {
             private final BufferedImage divider;
         }
 
-        private int x;
-        private int y;
+        private final ArrayList<PositionedImage> divider = new ArrayList<>();
+        private Point previousPosition = new Point();
+        private final Point position = new Point();
         @Setter
+        @Getter
         private int width;
-        @Getter(AccessLevel.NONE)
         private Divider.Horizontal.Type dividerType;
 
-        public void setPosition(int x, int y) {
-            this.x = x;
-            this.y = y;
+        public Horizontal(@Positive int x, @Positive int y, @Positive int width, @Nonnull Type dividerType) {
+            this.position.setLocation(x, y);
+            this.width = width;
+            this.dividerType = dividerType;
+        }
+
+        public Point getPosition() {
+            return new Point(position);
+        }
+
+        public void setPosition(@Positive int x, @Positive int y) {
+            position.setLocation(x, y);
+        }
+
+        @Override
+        public Dimension render(Graphics2D graphics) {
+            if (!position.equals(previousPosition)) {
+                previousPosition = new Point(position);
+                PiecesTool.Populator.populateHorizontalLine(divider, dividerType.getDivider(), position.x, position.y,
+                        width);
+            }
+            divider.forEach(entry -> entry.draw(graphics));
+            return null;
         }
     }
 }
